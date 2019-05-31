6a2707d8d31f32756f44fb0ac22cd59c3ae388c0
Janice Pena <jipena94@gmail.com>
2019-05-31 02:46:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,9 +3,11 @@ require 'pry'
 
 # require libraries/modules here
 
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
-# projects: kickstarter.css("li.project.grid_4")
