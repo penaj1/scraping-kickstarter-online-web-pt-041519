53da496b902be10f56a6bdace299ddd6f3197243
Janice Pena <jipena94@gmail.com>
2019-05-31 02:39:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,11 @@
+require 'nokogiri'
+require 'pry'
+
 # require libraries/modules here
 
 def create_project_hash
-  # write your code here
+  
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
